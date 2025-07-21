855fcd187ce7ad2560d3b22c6c0886634598c118
Zamahayise Hlatshwayo <zamahayise22@gmail.com>
2025-07-21 23:36:33 +0200

    Deploy pt. 2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "./App.css";
 
 function App() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename="/audiology-website">
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
