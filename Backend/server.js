a9ffb0e79f3dd126f2454a94632eea755c319f3d
Zamahayise Hlatshwayo <zamahayise22@gmail.com>
2025-11-09 15:24:32 +0200

    Updating server.js

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,9 @@ app.post("/send-email", async (req, res) => {
 	const { name, email, subject, message } = req.body;
 
 	const transporter = nodemailer.createTransport({
-		service: "gmail",
+		host: "smtp.gmail.com",
+		port: 587,
+		secure: false,
 		auth: {
 			user: process.env.EMAIL_USER,
 			pass: process.env.EMAIL_PASS,
